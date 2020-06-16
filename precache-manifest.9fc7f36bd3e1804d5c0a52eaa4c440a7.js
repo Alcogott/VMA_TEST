cf5fc5b8354e669380ae0b60552e8a3481f8c45d
Antonov Danil <d.antonv02@gmail.com>
2020-06-16 19:50:42 +0700

    Updates

---
+++ precache-manifest.9fc7f36bd3e1804d5c0a52eaa4c440a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15d90bd0bcd73f4d9c1ff5155d3b9a2",
+    "url": "/VMA_TEST/index.html"
+  },
+  {
+    "revision": "52aedeb78a0b06aca6ec",
+    "url": "/VMA_TEST/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9b7f3679dcd27d6a0474",
+    "url": "/VMA_TEST/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "52aedeb78a0b06aca6ec",
+    "url": "/VMA_TEST/static/js/2.d682c532.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/VMA_TEST/static/js/2.d682c532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7f3679dcd27d6a0474",
+    "url": "/VMA_TEST/static/js/main.918353fe.chunk.js"
+  },
+  {
+    "revision": "e4d7db9248c99b1cd594",
+    "url": "/VMA_TEST/static/js/runtime-main.c7422eae.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VMA_TEST/static/media/persik.4e1ec840.png"
+  }
+]);
